2a12cb3d6256f915c185d0e593d253f7504397fc
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-13 07:38:05 -0700

    validateScheme

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -33,7 +33,14 @@ try{
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const {scheme_name} = req.body
+if( scheme_name=== undefined || 
+  typeof scheme_name !== "string" || 
+  !scheme_name.trim() ){
+next({status:400, message:`invalid scheme_name`})
+}else{
+  next()
+}
 }
 
 /*
